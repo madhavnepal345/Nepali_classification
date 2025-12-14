cdf60dcf5ee55f6ed033ac8dc72ea584a63b8316
madhav <madhavnepal916@gmail.com>
2025-12-14 14:07:15 +0545

    changed the approach to train the datset

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,8 @@
 from datasets import load_dataset
 
 
-def load_data(dataset_name,train_samples=None,test_samples=None):
-    dataset=load_dataset(dataset_name)
+def load_data(train_samples=200000,test_samples=50000):
+    dataset=load_dataset("IRIIS-RESEARCH/Nepali-Text-Corpus")
     train_ds=dataset['train']
     test_ds=dataset['test']
 
@@ -11,3 +11,7 @@ def load_data(dataset_name,train_samples=None,test_samples=None):
     if test_samples:
         test_ds=test_ds.shuffle(seed=42).select(range(test_samples))
     return train_ds,test_ds
+
+
+def preprocess_text(dataset):
+    return dataset["Article"],dataset["Source"]
