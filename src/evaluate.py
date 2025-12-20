212da038515b0aeefc65bc1f24d0928161e84e8d
madhav <madhavnepal916@gmail.com>
2025-12-20 14:03:15 +0545

    evaluation metrices

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,9 +1,9 @@
-from sklearn.metrics import accuracy_score,precision_socre, recall_score, f1_score,f1_score
+from sklearn.metrics import accuracy_score,precision_score, recall_score, f1_score,f1_score
 
 def evaluate(y_true,y_pred):
     return{
         "accuracy":accuracy_score(y_true,y_pred),
-        "precision":precision_socre(y_true,y_pred),
+        "precision":precision_score(y_true,y_pred),
         "recall":recall_score(y_true,y_pred),
         "f1_score":f1_score(y_true,y_pred)
     }
